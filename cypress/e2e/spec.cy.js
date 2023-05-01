eb1d650835995586afb0ddc5253ba06f23c42242
Nicklas Christensen <Nicklas.chr.imp@gmail.com>
2023-05-01 11:14:41 +0200

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 //1. Lav en test, som kontrollerer, at brugeren kan tilføje en ny opgave til listen.
 describe('Add new project', () => {
   it('passes', () => {
-    cy.visit('http://localhost:8080/')
+    cy.visit('/')
     cy.get('#input').click().type('Cypress Test')
     cy.get('#butAdd').click()
     cy.get('.to-do-item .item')
@@ -11,7 +11,7 @@ describe('Add new project', () => {
 //2. Lav en test, som kontrollerer, at brugeren kan fjerne en opgave fra listen.
 describe('Delete new project', () => {
   it('passes', () => {
-    cy.visit('http://localhost:8080/')
+    cy.visit('/')
     cy.get('#input').click().type('Cypress Test')
     cy.get('#butAdd').click()
     cy.get('.fa-trash').click()
@@ -22,7 +22,7 @@ describe('Delete new project', () => {
 //3. Lav en test, som kontrollerer, at brugeren kan redigere en eksisterende opgave.
 describe('Edit new project', () => {
   it('passes', () => {
-    cy.visit('http://localhost:8080/')
+    cy.visit('/')
     cy.get('#input').click().type('Cypress Test')
     cy.get('#butAdd').click()
     cy.get('.fa-edit').click()
@@ -33,7 +33,7 @@ describe('Edit new project', () => {
 //4. Lav en test, som kontrollerer, at applikationen kan håndtere tilfældet, hvor brugeren forsøger at tilføje en tom opgave.
 describe('Empty new project', () => {
   it('passes', () => {
-    cy.visit('http://localhost:8080/')
+    cy.visit('/')
     cy.get('#input').click().type(' ')
     cy.get('#butAdd').click()
     cy.get('.to-do-item .item').should('not.exist')
